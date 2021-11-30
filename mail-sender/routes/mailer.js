60324cb092e52d45dcc81c34714f1766999daeb5
Apdo Elsaed <abdoelsaed114@gmail.com>
2021-11-30 14:30:50 +0200

    delete mail-sender service

--- mail-sender/routes/mailer.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-
-
-const { sendEmail } = require("../controllers/mailerController");
-
-router.post("/send", sendEmail);
-
-module.exports = router;
