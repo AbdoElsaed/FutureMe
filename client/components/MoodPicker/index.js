e8566d0876cabe0287fdfef151bf101d64515e10
Apdo Elsaed <abdoelsaed114@gmail.com>
2021-12-17 21:33:13 +0200

    scale emojis to 1.5 when clicked

--- client/components/MoodPicker/index.js
+++ client/components/MoodPicker/index.js
@@ -17,17 +17,28 @@ const MoodPicker = ({ mood, setMood }) => {
     { name: "meh", icon: faMeh },
   ];
   const handleMoodChange = (v, e) => {
-    setMood(v);
-    elements.forEach((i) => {
-      document.getElementById(i.name).style.color = "#2375e0e8";
-    });
-    document.getElementById(v).style.color = "chocolate";
+    // check if the clicked emoji is already active or not
+    if (document.getElementById(v).style.scale === "1.5") {
+      setMood("");
+      document.getElementById(v).style.color = "#2375e0e8";
+      document.getElementById(v).style.scale = "1";
+    } else {
+      setMood(v);
+      elements.forEach((i) => {
+        document.getElementById(i.name).style.color = "#2375e0e8";
+        document.getElementById(i.name).style.scale = "1";
+      });
+      document.getElementById(v).style.color = "#ff6a00";
+      document.getElementById(v).style.scale = "1.5";
+    }
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.moodDesc}>
-        {mood !== "" ? `${mood[0].toUpperCase()}${mood.slice(1)}` : "Your current mood:"}
+        {mood !== ""
+          ? `${mood[0].toUpperCase()}${mood.slice(1)}`
+          : "Pick Your Current Mood:"}
       </div>
       <div className={styles.iconsContainer}>
         {elements.map((elem) => (
